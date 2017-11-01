6fd74597db68469cc3ae128ba2248190d254b5ed
srinathboddu22 <srinathboddu22@gmail.com>
2017-11-01 11:11:58 +0530

    DAO Impl

---
+++ src/main/java/com/niit/dao/BlogPostDaoImpl.java
@@ -0,0 +1,72 @@
+package com.niit.dao;
+
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.model.BlogComment;
+import com.niit.model.BlogPost;
+
+
+@Repository
+public class BlogPostDaoImpl implements BlogPostDao
+{
+	@Autowired
+	private SessionFactory sessionFactory;
+	public void saveBlogPost(BlogPost blogPost) 
+	{
+		Session session=sessionFactory.openSession();
+		session.save(blogPost);
+		session.flush();
+		session.close();
+	}
+	
+	public List<BlogPost> getAllBlogs(int approved) 
+	{
+		Session session=sessionFactory.openSession();
+		Query query=session.createQuery("from BlogPost where approved="+approved);
+		List<BlogPost> blogPosts=query.list();
+		session.close();
+		return blogPosts;
+	}
+	
+	public BlogPost getBlogById(int id) 
+	{
+		Session session=sessionFactory.openSession();
+		BlogPost blogPost=(BlogPost)session.get(BlogPost.class, id);
+		session.close();
+		return blogPost;
+	}
+
+	public void updateBlogPost(BlogPost blogPost) {
+		
+		Session session=sessionFactory.openSession();
+		session.update(blogPost);
+		session.flush();
+		session.close();
+	}
+	
+public void addBlogComment(BlogComment blogComment) {
+		
+		Session session=sessionFactory.openSession();
+		session.save(blogComment);
+		session.flush();
+		session.close();
+	}
+public List<BlogComment> getBlogComments(int blogId) {
+    Session session=sessionFactory.openSession();
+  Query query=session.createQuery("from BlogComment where blogPost.id="+blogId);
+   
+    List<BlogComment> blogComments=query.list();
+    System.out.println("blogComments");
+    session.close();
+    return blogComments;
+    
+}
+
+}
---
+++ src/main/java/com/niit/dao/UsersDaoImpl.java
@@ -0,0 +1,77 @@
+ package com.niit.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.model.Users;
+@Repository
+public class UsersDaoImpl implements UsersDao
+{
+	
+@Autowired
+private SessionFactory sessionFactory;
+
+public void registration(Users users) 
+{
+	Session session=sessionFactory.openSession();
+
+	Transaction tx=session.beginTransaction();
+	users.setEnabled(true);
+	users.setOnline(false);
+	session.save(users);
+	tx.commit();
+	session.flush();
+	session.close();
+}
+	
+	public List<Users> getAllUsers() 
+	{
+		Session session=sessionFactory.openSession();
+		Query query=session.createQuery("from users");
+		List<Users> users=query.list();
+		session.close();
+		return users;
+	}
+	
+	public Users login(Users users)
+	{
+		System.out.println(users.getUsername()+""+users.getPassword());
+		Session session=sessionFactory.openSession();
+		System.out.println("hello");
+		Query query=session.createQuery("from Users where username=? and password=? and enabled=?");
+		System.out.println("hell");
+		query.setString(0, users.getUsername()); //for assigning the values to parameter username
+		query.setString(1, users.getPassword());
+		query.setBoolean(2, true);
+		Users validUsers=(Users)query.uniqueResult();
+		session.close();
+		System.out.println("Dao completed");
+		return validUsers;
+		
+	}
+	
+	public Users updateUser(Users validUser)
+	{
+		Session session=sessionFactory.openSession();
+		session.update(validUser);
+		session.flush();
+		session.close();
+		return validUser;
+	}
+
+	public Users getUserByUsername(int id)
+	{
+		Session session=sessionFactory.openSession();
+		Users users=(Users)session.get(Users.class, id);
+		session.close();
+		return users;
+	}
+
+	
+}
